d5f89e5ba5fef4ae42c05507059a952fe612a751
Sivateja <57987686+sivatejaa@users.noreply.github.com>
2023-04-14 10:47:07 -0500

    added changes

--- controllers/flowerController.js
+++ controllers/flowerController.js
@@ -11,9 +11,18 @@ exports.flower_list = async function(req, res) {
     } 
    };
 // for a specific flower.
-exports.flower_detail = function(req, res) {
- res.send('NOT IMPLEMENTED: flower detail: ' + req.params.id);
-};
+exports.flower_detail = async function(req, res) {
+    console.log("detail" + req.params.id)
+    try {
+    result = await Flower.findById( req.params.id)
+    res.send(result)
+    } catch (error) {
+    res.status(500)
+    res.send(`{"error": document for id ${req.params.id} not found`);
+    }
+   };
+
+
 // Handle flower create on POST.
 
 exports.flower_create_post = async function(req, res) {
@@ -40,9 +49,27 @@ exports.flower_delete = function(req, res) {
  res.send('NOT IMPLEMENTED: flower delete DELETE ' + req.params.id);
 };
 // Handle flower update form on PUT.
-exports.flower_update_put = function(req, res) {
- res.send('NOT IMPLEMENTED: flower update PUT' + req.params.id);
-}
+
+
+exports.flower_update_put = async function(req, res) {
+    console.log(`update on id ${req.params.id} with body
+   ${JSON.stringify(req.body)}`)
+    try {
+    let toUpdate = await Flower.findById( req.params.id)
+    // Do updates of properties
+    if(req.body.costume_type)
+    toUpdate.costume_type = req.body.costume_type;
+    if(req.body.cost) toUpdate.cost = req.body.cost;
+    if(req.body.size) toUpdate.size = req.body.size;
+    let result = await toUpdate.save();
+    console.log("Sucess " + result)
+    res.send(result)
+    } catch (err) {
+    res.status(500)
+    res.send(`{"error": ${err}: Update for id ${req.params.id}
+   failed`);
+    }
+   };
 
 exports.flower_view_all_Page = async function(req, res) {
     try{
