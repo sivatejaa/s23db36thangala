e6345af55daf721a8c1bbd8b0d4e6164c6ea3d88
Sivateja <57987686+sivatejaa@users.noreply.github.com>
2023-04-19 21:01:52 -0500

    added changes

--- controllers/flower_detail_controller.js
+++ controllers/flower_detail_controller.js
@@ -3,8 +3,7 @@ var Flower = require('../models/flower');
 exports.flower_view_one_Page = async function(req, res) {
     console.log("single view for id " + req.query.id)
     try{
-        const options = { maxTimeMS: 15000 };
-        result = await Flower.findById( req.query.id, options)
+        result = await Flower.findById( req.query.id)
         console.log("result " + result)
         res.render('flowerDetail',{ title: 'Flower Detail', toShow: result });
     }
