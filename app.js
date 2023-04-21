dbdff3bf8544c38decd3b41046175c69159dcca7
Sivateja <57987686+sivatejaa@users.noreply.github.com>
2023-04-21 10:39:20 -0500

    added changes

--- app.js
+++ app.js
@@ -4,6 +4,21 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+  Account.findOne({ username: username }, function (err, user) {
+  if (err) { return done(err); }
+  if (!user) {
+  return done(null, false, { message: 'Incorrect username.' });
+  }
+  if (!user.validPassword(password)) {
+  return done(null, false, { message: 'Incorrect password.' });
+  }
+  return done(null, user);
+  });
+  }));
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var flowersRouter = require('./routes/flowers');
@@ -33,6 +48,15 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.use(require('express-session')({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false
+ }));
+ app.use(passport.initialize());
+ app.use(passport.session());
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
@@ -42,6 +66,10 @@ app.use('/board', boardRouter);
 app.use('/selector', selectorRouter);
 app.use('/flower', Flower);
 app.use('/resourse', resourceRouter);
+var Account =require('./models/account');
+passport.use(new LocalStrategy(Account.authenticate()));
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser());
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
